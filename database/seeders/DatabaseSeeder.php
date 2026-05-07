fb0677cc480708a139af7485c47f8303daaa0329
Azamateek228 <azamatgizetdinov06@gmail.com>
2026-05-07 23:27:14 +0300

    Finalize tour navigation and afisha route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,25 +18,26 @@ class DatabaseSeeder extends Seeder
     {
         $routeCities = [
             ['name' => 'Набережные Челны', 'lat' => 55.7436, 'lng' => 52.3958, 'population' => 548434],
+            ['name' => 'Менделеевск', 'lat' => 55.8952, 'lng' => 52.3144, 'population' => 22442],
             ['name' => 'Елабуга', 'lat' => 55.7567, 'lng' => 52.0544, 'population' => 74390],
             ['name' => 'Нижнекамск', 'lat' => 55.6361, 'lng' => 51.8245, 'population' => 241329],
+            ['name' => 'Мамадыш', 'lat' => 55.7149, 'lng' => 51.4070, 'population' => 15752],
+            ['name' => 'Заинск', 'lat' => 55.3207, 'lng' => 52.0669, 'population' => 41498],
             ['name' => 'Альметьевск', 'lat' => 54.9014, 'lng' => 52.2971, 'population' => 152580],
             ['name' => 'Лениногорск', 'lat' => 54.6020, 'lng' => 52.4609, 'population' => 60878],
             ['name' => 'Бугульма', 'lat' => 54.5364, 'lng' => 52.7895, 'population' => 81744],
             ['name' => 'Азнакаево', 'lat' => 54.8579, 'lng' => 53.0698, 'population' => 32926],
-            ['name' => 'Заинск', 'lat' => 55.3207, 'lng' => 52.0669, 'population' => 41498],
-            ['name' => 'Менделеевск', 'lat' => 55.8952, 'lng' => 52.3144, 'population' => 22442],
-            ['name' => 'Мамадыш', 'lat' => 55.7149, 'lng' => 51.4070, 'population' => 15752],
             ['name' => 'Мензелинск', 'lat' => 55.7271, 'lng' => 53.1026, 'population' => 16948],
         ];
 
-        $cities = collect($routeCities)->mapWithKeys(function (array $city) {
+        $cities = collect($routeCities)->mapWithKeys(function (array $city, int $index) {
             $model = City::updateOrCreate(
                 ['name' => $city['name']],
                 [
                     'lat' => $city['lat'],
                     'lng' => $city['lng'],
                     'population' => $city['population'],
+                    'route_order' => $index + 1,
                 ]
             );
 
